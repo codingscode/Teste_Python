9b47ab395f45ebf6c4262ed7ceadc027c0652196
codingscode <heronnestg@hotmail.com>
2021-03-26 23:25:05 -0300

    parte 4

---
+++ 01/teste004.py
@@ -0,0 +1,53 @@
+numero1 = 9
+numero2 = 8.56
+numero3 = 7j
+string1 = '4'
+string2 = '4.5'
+
+print(f'numero1: {numero1}, numero2: {numero2}, numero3: {numero3}')
+print(f'numero1 tipo: {type(numero1)}, numero2 tipo: {type(numero2)}, numero3 tipo: {type(numero3)}')
+print('------------------------------')
+
+# convertendo para int
+valor1 = 23.45
+print(valor1)
+print(int(valor1))
+print(string1)
+print(int(string1))
+
+# convertendo para float
+valor2 = 18
+print(valor2)
+print(float(valor2))
+print(string2)
+print(float(string2))
+
+# convertendo para complexo
+valor3 = 7
+print(valor3)
+print(complex(valor3))
+
+from random import randrange
+
+print(randrange(4, 10)) # inteiro aleatorio de 4 até 10-1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01/teste005.py
@@ -0,0 +1,15 @@
+
+valor1 = 'casa'
+valor2 = 5
+valor3 = 7.2
+valor4 = 1 +9j
+
+print(f'valor1: {valor1}')
+print(f'valor2: {valor2}')
+print(f'valor3: {valor3}')
+print(f'valor4: {valor4}')
+
+print(str(valor1))
+print(str(valor2))
+print(str(valor3))
+print(str(valor4))
