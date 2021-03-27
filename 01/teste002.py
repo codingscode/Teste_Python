efc77cb1eec6886551abd6eb86ac1e8c61090600
codingscode <heronnestg@hotmail.com>
2021-03-26 22:53:02 -0300

    parte 2

---
+++ 01/teste002.py
@@ -0,0 +1,33 @@
+
+prod1 = 'pendrive'
+preco = 30.40
+quantidade = 2
+
+print(type(prod1))
+print(type(preco))
+print(type(quantidade))
+
+print('-----------------------------')
+
+a, b = 'primeiro', 'depois'
+
+c, d = 10, 5
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
