814f4bed3031690d1173f82439e354a0c861f989
codingscode <heronnestg@hotmail.com>
2021-03-28 23:45:19 -0300

    parte 10

---
+++ 01/teste014.py
@@ -0,0 +1,21 @@
+compras = ['uva', 'melão', 'pão', 'carne']
+print(compras)
+print(compras[0])
+print(compras[1])
+
+compras[0] = 'frango'
+
+print(compras)
+print(len(compras))
+print('-----------------------')
+
+lista = [1, True, 'olá', 6.2]  # lista pode conter vários tipos
+print(lista)
+print(type(lista))
+
+string1 = 'pássaro'
+lista2 = list(string1)  # converte para tipo lista
+
+print(lista2)
+
+print('-----------------------')
