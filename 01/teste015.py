105c680489c2ab1859d012786d722fa4b476039f
codingscode <heronnestg@hotmail.com>
2021-03-29 20:17:56 -0300

    parte 11

---
+++ 01/teste015.py
@@ -0,0 +1,17 @@
+cores = ['verde', 'azul', 'violeta', 'vermelho', 'amarelo', 'branco', 'preto', 'roxo', 'laranja']
+
+
+print(cores[0])
+print(cores[-1])
+print(cores[-2])
+print(cores[2:5])
+print(cores[3:])
+print(cores[:3])
+print(cores[::3])
+
+if 'marrom' in cores:
+    print('marrom existe na lista cores')
+else:
+    print('marrom não está na lista cores')
+
+print('------------------------------')
