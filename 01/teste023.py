5d1dd2e9b983e264d9c5dab7be8f9f5397252bb7
codingscode <heronnestg@hotmail.com>
2021-04-01 12:37:12 -0300

    parte 18

---
+++ 01/teste023.py
@@ -0,0 +1,26 @@
+lista1 = [3, 9, 'oi', -5, True]
+copia1_lista1 = lista1
+
+
+
+print(f'lista1: {lista1}')
+print(f'copia1_lista1: {copia1_lista1}')
+
+lista1 = [27, False, 'mudado', 2]
+print(f'lista1: {lista1}')
+print(f'copia1_lista1: {copia1_lista1}')
+
+print('---------------------------------')
+cores = ['verde', 'vermelho', 'roxo']
+cores_copia = cores.copy()
+
+
+print(f'cores: {cores}')
+print(f'cores_copia: {cores_copia}')
+
+cores = ['amarelo', 'laranja', 'branco']
+
+print(f'cores: {cores}')
+print(f'cores_copia: {cores_copia}')
+
+print('---------------------------------')
