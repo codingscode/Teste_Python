07e2f19698560fea34163aac79d686a57cb24e1d
codingscode <heronnestg@hotmail.com>
2021-04-03 22:53:57 -0300

    parte 21

---
+++ 01/teste026.py
@@ -0,0 +1,17 @@
+dias_semana = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado']
+
+
+print(dias_semana)
+print(dias_semana[0])
+print(dias_semana[2])
+print(dias_semana[::2])
+print(dias_semana[::-1])
+print(dias_semana[3:6])
+
+print('------------------------')
+if 'quarta' in dias_semana:
+    print('quarta está presente')
+else:
+    print('quarta não está presente')
+
+print('------------------------')
