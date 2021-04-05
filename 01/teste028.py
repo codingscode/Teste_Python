25a3e1d5fbc3ab519022182fc360bd0dc959418f
codingscode <heronnestg@hotmail.com>
2021-04-04 22:11:12 -0300

    parte 23

---
+++ 01/teste028.py
@@ -0,0 +1,37 @@
+dias_semana = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado']
+
+
+print(dias_semana)
+print(dias_semana[-1])
+print(dias_semana[-2])
+print(dias_semana[1:5])
+
+print('------------------------')
+cores1 = ('verde', 'azul', 'branco')
+cores2 = ('roxo', 'laranja')
+cores = cores1 + cores2
+
+
+print(cores1)
+print(cores2)
+print(cores)
+
+print('------------------------')
+roupa = ('camisa', 'calça')
+novo = roupa * 3
+
+print(roupa)
+print(novo)
+
+print('------------------------')
+numeros = (3, 4, 8, 5, 2, 1, 8, 9, 0, 8)
+
+print(numeros)
+
+quantos = numeros.count(8)
+print(quantos)
+
+indice = numeros.index(8)
+print(indice)
+
+print('------------------------')
