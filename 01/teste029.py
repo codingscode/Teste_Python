543a65aa5fc2d30a51f9c73a84b94db48081a082
codingscode <heronnestg@hotmail.com>
2021-04-04 22:55:36 -0300

    parte 24

---
+++ 01/teste029.py
@@ -0,0 +1,23 @@
+meses = {'janeiro', 'março', 'dezembro', 'setembro'}
+notas = {20, 200, 5, 50, 100, 2, 200}
+
+
+print(meses)
+print(notas)
+
+# sets são não ordenados, não modificados, duplicados não permitidos
+
+print('------------------------')
+print(len(meses))
+print(type(meses))
+print(len(notas))
+
+
+print('------------------------')
+lugares = ['cuiabá', 'curitiba', 'recife', 'sobral', 'maceió']
+para_set = set(lugares)
+
+print(lugares)
+print(para_set)
+
+print('------------------------')
