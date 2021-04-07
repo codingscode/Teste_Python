74c4d09222fc6dbdbfceecefede18e59c89bcf12
codingscode <heronnestg@hotmail.com>
2021-04-06 22:00:05 -0300

    parte 28

---
+++ 01/teste033.py
@@ -0,0 +1,15 @@
+# dicionario é ordenavel, mutavel e nao permite duplicatas
+produto = {'nome': 'cabo hdmi', 'preco': 44.55, 'quantidade': 1, 'frete': 2.5}
+
+print(produto)
+print(len(produto))
+print(type(produto))
+print(produto['nome'])
+print(produto['preco'])
+
+print('-------------------')
+pessoa = {'nome': 'david', 'idade': 20, 'estado': 'paraná', 'estado': 'piauí'}
+
+print(pessoa)
+
+print('-------------------')
