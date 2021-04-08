06f0afad91876f75fea19f249fc6f1c0000e6b1e
codingscode <heronnestg@hotmail.com>
2021-04-07 21:50:24 -0300

    parte 31

---
+++ 01/teste037.py
@@ -0,0 +1,16 @@
+pessoa = { 'nome': 'Fabiola', 'idade': 19, 'massa': 73, 'altura': 1.77, 'uf': 'tocantis' }
+pessoa2 = pessoa.copy()
+
+print(f'pessoa: {pessoa}')
+print(f'pessoa2: {pessoa2}')
+
+print('--------------------')
+compra = {
+  'produto1' : { 'nome' : 'pendrive', 'preco' : 25.5, 'frete': 3.75 },
+  'produto2' : { 'nome' : 'tv', 'preco' : 500, 'frete': 40.23 },
+  'produto3' : { 'nome' : 'play2', 'preco' : 900, 'frete': 30.25 }
+}
+
+print(compra)
+
+print('--------------------')
