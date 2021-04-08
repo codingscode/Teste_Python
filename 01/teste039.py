6909f5a7a42b076586e43cbadc6126f5ee7a5da1
codingscode <heronnestg@hotmail.com>
2021-04-08 09:59:23 -0300

    parte 33

---
+++ 01/teste039.py
@@ -0,0 +1,33 @@
+i = 0
+
+while i < 6:
+    print(i)
+    i += 1
+
+print('-----------------------------')
+i2 = 0
+
+while i2 < 6:
+    print(i2)
+    if i2 == 3:
+        break
+    i2 += 1
+
+print('-----------------------------')
+i3 = 0
+
+while i3 < 6:
+    i3 += 1
+    if i3 == 3:
+        continue
+    print(i3)
+
+print('-----------------------------')
+i4 = 0
+while i4 < 6:
+  print(i4)
+  i4 += 1
+else:
+  print('i4 não é menor que 6 mais')
+
+print('-----------------------------')
