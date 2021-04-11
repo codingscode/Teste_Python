6cd1f378b3826dcf51fd91ad5cd4568ecdf2ffb8
codingscode <heronnestg@hotmail.com>
2021-04-11 19:57:41 -0300

    parte 44

---
+++ 01/teste050.py
@@ -0,0 +1,52 @@
+iteravel = [10, 2, 7, -3, 20, 8]
+
+maior = max(iteravel)
+menor = min(iteravel)
+
+print(iteravel)
+print(f'maior: {maior}')
+print(f'menor: {menor}')
+
+print('---------------------')
+numero = -47.25
+absoluto = abs(numero)
+
+print(f'numero: {numero}')
+print(f'absoluto: {absoluto}')
+
+print('---------------------')
+base = 5
+potencia = 3
+
+numero2 = pow(base, potencia)
+
+print(f'numero2: {numero2}')
+print(5**3)
+
+print('---------------------')
+from math import sqrt, ceil, floor, pi
+
+
+raiz_quadrada = sqrt(64)
+
+print(f'raiz quadrada: {raiz_quadrada}')
+print(64**0.5)
+
+print('---------------------')
+numero3 = 5.6
+
+print(f'numero3: {numero3}')
+print(ceil(numero3))
+print(floor(numero3))
+print(f'pi: {pi}')
+
+print('---------------------')
+
+
+print('---------------------')
+
+
+
+print('---------------------')
+
+
