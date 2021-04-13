afa943c4ba280ed6163d9ee46946d73bb7a1ed64
codingscode <heronnestg@hotmail.com>
2021-04-13 10:49:50 -0300

    parte 52

--- 01/teste055.py
+++ 01/teste055.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 #print(dir(pd))  # o que é possível fazer
-arquivo = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/dados2.csv')
+pd.options.display.width = None
+arquivo = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/dados.csv')
 
-print(arquivo.to_string())
+print(arquivo.head())
 
 print('---------------------')
 
