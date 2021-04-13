388041471aa7d35ee4c939b725c7a3d6f0d761ea
codingscode <heronnestg@hotmail.com>
2021-04-13 09:44:32 -0300

    parte 49

---
+++ 01/teste054.py
@@ -0,0 +1,9 @@
+nome = input('digite seu nome: \n')
+print(f'olá {nome} !')
+
+print('-------------------------')
+preco = 48
+
+print(f'preco: {preco:.2f}')  # 2 casas decimais
+
+print('-------------------------')
