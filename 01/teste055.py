4414ca5f645ba040e4355c1d7558938a99bf2c70
codingscode <heronnestg@hotmail.com>
2021-04-13 10:25:00 -0300

    parte 50

---
+++ 01/teste055.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+#print(dir(pd))  # o que é possível fazer
+arquivo = pd.read_csv('https://drive.google.com/file/d/1IISKLkMq2rz2bzVGr5BoYU0_BXLUeaUh/view?usp=sharing')
+#arquivo = pd.read_csv('https://drive.google.com/file/d/1Tn7-yxcQuPvf6-fGKGhRbN0GBTtelgA5/view?usp=sharing')
+
+print(arquivo.to_string())
+
+"""
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+print(df.to_string())
+"""
+
+print('---------------------')
+
+
+
+
+print('---------------------')
+
+
+
+
+print('---------------------')
+
+
+
+
+print('---------------------')
+
+
+
+print('---------------------')
+
+
+
+
+print('---------------------')
