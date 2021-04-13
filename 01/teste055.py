14d65494ca3b3ddfeeb91147db2cb96e27d2e181
codingscode <heronnestg@hotmail.com>
2021-04-13 10:39:39 -0300

    parte 51

--- 01/teste055.py
+++ 01/teste055.py
@@ -1,18 +1,9 @@
 import pandas as pd
 
 #print(dir(pd))  # o que é possível fazer
-arquivo = pd.read_csv('https://drive.google.com/file/d/1IISKLkMq2rz2bzVGr5BoYU0_BXLUeaUh/view?usp=sharing')
-#arquivo = pd.read_csv('https://drive.google.com/file/d/1Tn7-yxcQuPvf6-fGKGhRbN0GBTtelgA5/view?usp=sharing')
+arquivo = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/dados2.csv')
 
 print(arquivo.to_string())
-
-"""
-import pandas as pd
-
-df = pd.read_csv('data.csv')
-
-print(df.to_string())
-"""
 
 print('---------------------')
 
