6dc7a229616621ed25e2a50c1a771cec2ecaba42
codingscode <heronnestg@hotmail.com>
2021-04-14 10:44:24 -0300

    parte 55

---
+++ 01/teste058.py
@@ -0,0 +1,11 @@
+# usar colab
+
+import pandas as pd
+
+mostrar1 = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/dados.csv')
+mostrar1.to_string()
+
+print('--------------------------')
+mostrar1.head()
+
+print('--------------------------')
