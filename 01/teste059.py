959664eaf094865393d07e7ae7806ae2e5827fee
codingscode <heronnestg@hotmail.com>
2021-04-15 10:12:59 -0300

    parte 57

--- 01/teste059.py
+++ 01/teste059.py
@@ -1,25 +1,22 @@
 # usar colab
+import pandas as pd
+from json import loads, dumps
+from urllib.request import urlopen
 
 
+mostrar1 = pd.read_json('https://raw.githubusercontent.com/codingscode/Teste_Python/master/meu_json.json')
+mostrar2 = mostrar1.to_string()
 
-
-print('--------------------------------')
-
-
-
-
-print('--------------------------------')
-
-
-
+mostrar1
+mostrar2
 
 print('--------------------------------')
-
-
+mostrar3 = mostrar1.head()
+mostrar3
 
 print('--------------------------------')
-
-
-
+resposta = urlopen('https://raw.githubusercontent.com/codingscode/Teste_Python/master/meu_json.json')
+dados_json = loads(resposta.read())
+dados_json
 
 print('--------------------------------')
