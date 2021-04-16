2ab83ae7d8635e21343e4fa388a80c847abccb14
codingscode <heronnestg@hotmail.com>
2021-04-16 10:11:41 -0300

    parte 60

--- 01/teste061.py
+++ 01/teste061.py
@@ -3,19 +3,14 @@ import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/dados.csv')
 
-df.head()
-df.head(2)
+df  # aparece todos
+df.head()  # aparece só 5 primeiros
+df.head(3)  # aparece só 3 primeiros
 
 print('--------------------------------')
-df.tail()
-
-
-print('--------------------------------')
-
-
+df.tail()  # aparece os 5 últimos
 
 print('--------------------------------')
-
-
+df.info()
 
 print('--------------------------------')
