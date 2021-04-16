f89408c6dd95d866a0e458edce58e4736c31f300
codingscode <heronnestg@hotmail.com>
2021-04-16 10:52:21 -0300

    parte 66

--- 01/teste062.py
+++ 01/teste062.py
@@ -3,7 +3,18 @@ import pandas as pd
 
 dados = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/dados.csv')
 
-
 dados
+print('------------------')
 novo_dados = dados.dropna()
-novo_dados.to_string()
+novo_dados
+
+print('------------------')
+novo_dados.dropna(inplace = True)
+novo_dados
+
+print('------------------')
+dados2 = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/dados.csv')
+dados2.fillna(130, inplace = True)
+dados2
+
+print('------------------')
