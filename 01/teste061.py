0d58e77b8c6f2069ef2ff13eb9f67e365c0c0a9f
codingscode <heronnestg@hotmail.com>
2021-04-16 10:01:16 -0300

    parte 59

---
+++ 01/teste061.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/dados.csv')
+
+df.head()
+df.head(2)
+
+print('--------------------------------')
+df.tail()
+
+
+print('--------------------------------')
+
+
+
+print('--------------------------------')
+
+
+
+print('--------------------------------')
