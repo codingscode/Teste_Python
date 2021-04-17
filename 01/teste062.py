aeb593002ce1888c6a96dd88c9b907c75a08c59f
codingscode <heronnestg@hotmail.com>
2021-04-17 09:36:38 -0300

    parte 67

--- 01/teste062.py
+++ 01/teste062.py
@@ -9,12 +9,58 @@ novo_dados = dados.dropna()
 novo_dados
 
 print('------------------')
-novo_dados.dropna(inplace = True)
-novo_dados
-
-print('------------------')
 dados2 = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/dados.csv')
-dados2.fillna(130, inplace = True)
-dados2
+novo_dados2 = dados2
+novo_dados2.dropna(inplace = True)
+novo_dados2
 
 print('------------------')
+dados3 = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/dados.csv')
+dados3.fillna(130, inplace = True)  # celulas vazias serão preenchidas por 130
+dados3
+
+print('------------------')
+
+
+
+
+
+
+print('------------------')
+
+
+
+
+
+print('------------------')
+
+
+
+
+
+
+print('------------------')
+
+
+
+
+
+
+print('------------------')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
