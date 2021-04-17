7023a0e7cc195c091c288c6f948142ff230b4afd
codingscode <heronnestg@hotmail.com>
2021-04-17 10:06:13 -0300

    parte 68

--- 01/teste062.py
+++ 01/teste062.py
@@ -21,16 +21,29 @@ dados3
 
 print('------------------')
 
-
-
-
-
+dados4 = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/dados.csv')
+dados4['sobrenome'].fillna('preenchido', inplace = True) # preenche somente os vazios da coluna 'sobrenome'
+dados4
 
 print('------------------')
 
+dados5 = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/dados.csv')
+coluna_idade = dados5['idade'].median()
+coluna_idade
+dados5['idade'].fillna(coluna_idade, inplace = True)
+dados5
 
 
 
+""" 
+
+df = pd.read_csv('data.csv')
+
+x = df["Calories"].median()
+
+df["Calories"].fillna(x, inplace = True)
+
+ """
 
 print('------------------')
 
