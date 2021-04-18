ea462a6220495b274e944d5311caeca2e4669858
codingscode <heronnestg@hotmail.com>
2021-04-18 12:02:21 -0300

    parte 79

--- 01/teste063.py
+++ 01/teste063.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 dados = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/dados.csv')
 
-
 dados
+
 dados['data'] = pd.to_datetime(dados['data'])
 dados
 
