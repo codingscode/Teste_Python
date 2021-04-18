f3863f97fc1ef85cf94b6f87b32f08f81fced34b
codingscode <heronnestg@hotmail.com>
2021-04-18 11:09:00 -0300

    parte 71

---
+++ 01/teste063.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+
+dados = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/dados.csv')
+
+
+dados
+dados['data'] = pd.to_datetime(dados['data'])
+dados
+
+
+
+""" 
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+
+df['Date'] = pd.to_datetime(df['Date'])
+
+print(df.to_string())
+
+"""
+
+print('-------------------')
+
+
+
+
+print('-------------------')
+
+
+
+
+
+print('-------------------')
+
+
+
+
+print('-------------------')
