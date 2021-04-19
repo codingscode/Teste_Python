6456a565e1190379e9bbdd8d1ae8d3dccc8555ec
codingscode <heronnestg@hotmail.com>
2021-04-19 09:49:37 -0300

    parte 84

---
+++ 01/teste067.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+dados = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/dados.csv')
+dados
+
+print('--------------------------')
+
+dados.plot()
+
+plt.show()
+
+
+
+print('--------------------------')
+
+dados2 = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/outros.csv')
+
+dados2  # imprimir
+dados2.plot()  # imprimir
+
+print('--------------------------')
+
+
+
+
+
+print('--------------------------')
+
+
+
+
+
+print('--------------------------')
