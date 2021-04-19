1ac43c746454f72dab9d80cb240475049c3d22e2
codingscode <heronnestg@hotmail.com>
2021-04-19 09:41:12 -0300

    parte 83

---
+++ 01/teste066.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+dados = pd.read_csv('https://raw.githubusercontent.com/codingscode/Teste_Python/master/dados.csv')
+dados
+
+print('----------------------')
+
+dados.corr()  # ignora valores não numéricos
+
+print('----------------------')
+
+
+
+
+
+print('----------------------')
+
+
+
+
+
+print('----------------------')
